c6c5539b09b365c64d0089ce3b8e218dfdc4b16e
Mickey Gousset <mickeygousset@github.com>
2023-10-18 16:13:32 -0500

    initial version

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.10" // <1>
+
+    application // <2>
+}
+
+repositories {
+    mavenCentral() // <3>
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5") // <4>
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3") // <5>
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("com.google.guava:guava:32.1.1-jre") // <6>
+}
+
+application {
+    mainClass.set("demo.AppKt") // <7>
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform() // <8>
+}
